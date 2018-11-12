82dcec700b7248fa2598d8ecad809f09dc8a48e4
rojrun <runroj@gmail.com>
2018-11-11 19:32:29 -0800

    Memory Match version0.5 - Roj Rungsisullatanont

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
-$(document).ready(startMemoryGame);
+$(document).ready(start_memory_game);
 
-function startMemoryGame(){
-    cardCreation();
+var first_card_clicked = null;
+var second_card_clicked = null;
+var total_possible_matches = 2;
+var match_counter = 0;
+
+var images = [
+    "./Rick_and_Morty/birdperson_rick.jpg",
+    "./Rick_and_Morty/citadel.jpg",
+    "./Rick_and_Morty/Evil-Rick.jpg",
+    "./Rick_and_Morty/Evil_Morty.jpg",
+    "./Rick_and_Morty/first_episode.jpg",
+    "./Rick_and_Morty/Pickle_Rick.jpg",
+    "./Rick_and_Morty/Rick_in_prison.jpg",
+    "./Rick_and_Morty/season-3.jpg",
+    "./Rick_and_Morty/Vindicators.jpg",
+];
 
 
+function start_memory_game(){
+    card_creation();
+    card_selection();
 }
 
-function cardCreation() {
+function card_creation() {
    for  (var cardRowIndex = 1; cardRowIndex <=3; cardRowIndex++) {
        var cardRow = $("<div>").addClass("cardRow");
        for (var cardIndex = 1; cardIndex <= 6; cardIndex++) {
            var playingCardFront = $("<div>").addClass("front");
-           var playingCardBack = $("<div>").addClass("back");
-           var divPlayingCard = $("<div>").addClass("card").text(cardIndex);
+           var playingCardBack = $("<div>").addClass("back").attr('cardIndex', cardIndex);
+           var divPlayingCard = $("<div>").addClass("card");
            $(divPlayingCard).append(playingCardFront, playingCardBack);
            $(cardRow).append(divPlayingCard);
        }
@@ -22,31 +39,59 @@ function cardCreation() {
 
 
 
+function card_selection() {
+    $(".back").click(card_clicked);
+}
+
+function card_clicked(){
+    // debugger;
+    console.log("clicked");
+    $(this).addClass("hidden");
+    if (first_card_clicked === null){
+        first_card_clicked = $(this);
+        console.log('First Card:', first_card_clicked);
+        console.log("first card clicked" , first_card_clicked.attr('cardIndex'));
+    } else {
+        second_card_clicked = $(this);
+        console.log("second card ", second_card_clicked.attr('cardIndex'));
+        if (first_card_clicked.attr('cardIndex') === second_card_clicked.attr('cardIndex')) {
+            console.log("both cards match");
+            match_counter++;
+            console.log("match counter ", match_counter);
+            setTimeout(function () {
+                first_card_clicked.remove(".front");
+                second_card_clicked.remove(".front");
+                first_card_clicked = null;
+                second_card_clicked = null;
+            }, 2000);
+            // first_card_clicked.removeClass("hidden");
+            // second_card_clicked.removeClass("hidden");
+            // first_card_clicked = null;
+            console.log("first_card_clicked = ", first_card_clicked);
+            // second_card_clicked = null;
+            console.log("second_card_clicked = ", second_card_clicked);
+            $(".title").text("Great job Morty!");
+            console.log("winner");
+            if (match_counter === total_possible_matches) {
+                $(".title").text("Great job Morty!");
+
+            } else {
+                console.log("loser");
+                return card_selection();
+                // setTimeout(function () {
+                // first_card_clicked.removeClass("hidden");
+                // second_card_clicked.removeClass("hidden");
+                // first_card_clicked = null;
+                // second_card_clicked = null;
+                // }, 2000);
+            }
+        }
+    }
+}
 
 
 
-//
-//
-// var firstCard= null;
-// var secondCard = null;
-// $(document).ready(startApp);
-//
-// startApp(){
-//     addclickhandlerstoelecmnets();
-// }
-// function addclickhandlerstolements(){
-//     $(".card").click(processcardclick);
-// }
-// function processcardclick(){
-//     console.log("process card click");
-//     $(this).addClass("hidden");
-//     if (firstCard === null){
-//         //this must be the first click
-//         firstCard = this;
-//         console.log("first")
-//     } else {
-//         console.log("second");
-//         secondCard= this;
-//         var firstCardBackground = $(firstCard).find('front').css("background-image");
-//     }
-// }
+
+
+
+
