1a5bc63ab34d1451dd8b7ea001c9d556de140122
rojrun <runroj@gmail.com>
2019-04-13 22:20:31 -0700

    revising cardclicked function

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-$(document).ready(start_memory_game);
+$(document).ready(start_memory_match_game);
 
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
+var total_possible_matches = 9;
 var match_counter = 0;
 var matches = 0;
 var attempts = 0;
@@ -22,40 +22,103 @@ var images = [
     "./Rick_and_Morty/Vindicators.jpg",
 ];
 
+var newDeck = shuffle_array(images);
 
-function start_memory_game(){
-    card_creation();
-    card_selection();
-    $(".reset").click(function(){
-       games_played++;
-       reset_stats();
-       display_stats();
-       $(".back").removeClass("hidden");
-       $(".title").text("Morty Smith is a moron!");
-    });
+function start_memory_match_game(){
+    shuffle_array(images);
+    card_creation(newDeck);
+    $(".reset").click(reset_game);
 }
 
-function card_creation() {
-   var doubled_images = images.concat( images );
-   for  (var cardRowIndex = 1; cardRowIndex <=3; cardRowIndex++) {
-       var cardRow = $("<div>").addClass("cardRow");
-       for (var cardIndex = 1; cardIndex <= 6; cardIndex++) {
-           var card_image = doubled_images.pop();
-           var playingCardFront = $("<div>").addClass("front").css("background-image", "url(" + card_image+ ")");
-           var playingCardBack = $("<div>").addClass("back");
-           var divPlayingCard = $("<div>").addClass("card");
-           $(divPlayingCard).append(playingCardFront, playingCardBack);
-           $(cardRow).append(divPlayingCard);
-       }
-       $(".game-area").append(cardRow);
-   }
+function shuffle_array(imagesArray) {
+    var doubled_images = imagesArray.concat( imagesArray );
+
+    for (var i = doubled_images.length; i; i -= 1) {
+        var randomize_images = Math.floor(Math.random() * i);
+        var x = doubled_images[i - 1];
+        doubled_images[i - 1] = doubled_images[randomize_images];
+        doubled_images[randomize_images] = x;
+    }
+
+    return doubled_images;
 }
 
-function card_selection() {
-    $(".back").click(card_clicked);
+function card_creation(shuffledDeck) {
+    for (var cardIndex = 0; cardIndex < shuffledDeck.length; cardIndex++) {
+        var playingCardFront = $("<div>").addClass("front").css("background-image", "url(" + shuffledDeck[cardIndex] + ")");
+        var playingCardBack = $("<div>").addClass("back").click(card_clicked);
+        var playingCardContainer = $("<div>").addClass("card");
+        $(playingCardContainer).append(playingCardFront, playingCardBack);
+        $(".game-area").append(playingCardContainer);
+    }
 }
 
 function card_clicked(){
+
+    if (first_card_clicked === null){
+        first_card_clicked = $(this);
+        flipCard(this);
+        first_card_clicked.siblings('.card-back').off();
+    } else{
+        second_card_clicked = $(this);
+        flipCard(this);
+        var firstCard = first_card_clicked.siblings('.card-back').css('background-image');
+        var secondCard = second_card_clicked.siblings('.card-back').css('background-image');
+        if (firstCard === secondCard){
+            //Found a match
+            var matchNumber = $('.matches .value');
+            match_counter++;
+            matchNumber.text(match_counter + ' ');
+            attempts++;
+            var attemptsValue=$('.attempts .value');
+            attemptsValue.text(attempts + ' ');
+            accuracy = match_counter/attempts;
+            var accuracyValue= $('.accuracy .value');
+            accuracyValue.text(Math.floor(accuracy * 100)+'%');
+            var cards = $('.card-front, .card-back');
+            cards.off('click');
+            setTimeout(function(){
+                pairMatchAnimation(first_card_clicked, second_card_clicked);
+                cards.on('click', clickCard);
+                first_card_clicked = null;
+                second_card_clicked = null;
+            },1200);
+            if (match_counter === matches){
+                //All matches found
+                winningDisplay();
+            }
+        } else {
+            var cards = $('.card-front, .card-back');
+            cards.off('click');
+            flag = true;
+            second_card_clicked.siblings('.card-back').addClass('shake')
+            first_card_clicked.siblings('.card-back').addClass('shake')
+            setTimeout(function(){
+                nonPairMatchAnimation();
+                cards.on('click', clickCard);
+                second_card_clicked.siblings('.card-back').removeClass('shake')
+                first_card_clicked.siblings('.card-back').removeClass('shake')
+                first_card_clicked = null;
+                second_card_clicked = null;
+            },700);
+            attempts++;
+            var attemptsValue=$('.attempts .value');
+            attemptsValue.text(attempts + ' ');
+
+            if(match_counter >= 0 && match_counter < 9){
+                accuracy = match_counter/attempts;
+                var accuracyValue= $('.accuracy .value');
+                accuracyValue.text(Math.floor(accuracy * 100)+'%');
+            } else{
+                winningDisplay();
+            }
+        }
+    }
+
+
+
+
+    // $(this).find(".back").hide();
     $(this).addClass("hidden");
     if (first_card_clicked === null){
         first_card_clicked = $(this);
@@ -98,6 +161,17 @@ function card_clicked(){
     }
 }
 
+function reset_game() {
+    games_played++;
+    reset_stats();
+    display_stats();
+    $(".back").removeClass("hidden");
+    $(".title").text("Morty Smith is a moron!");
+    $(".card").remove();
+    shuffle_array(images);
+    card_creation(newDeck);
+}
+
 function display_stats(){
     $(".games-played .value").text(games_played);
     $(".attempts .value").text(attempts);
@@ -111,9 +185,3 @@ function reset_stats(){
     attempts = 0;
     display_stats();
 }
-
-
-
-
-
-
